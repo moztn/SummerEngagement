a6b822756adbb69390cc2c4dc14cb55ec6d31c88
Alexandre BM <s@rednaks.tn>
2014-06-15 00:33:43 +0100

    Addin method to convert an object to dictionnary

--- src/models/entities.py
+++ src/models/entities.py
@@ -1,10 +1,15 @@
 from common import *
-  
+
 class Checkin(SQLObject):
   duration = IntCol()
   note = StringCol()
   checkinDate = DateTimeCol()
   mozillian = ForeignKey('Mozillian')
+
+  def toDict(self):
+    return {'duration':self.duration, 'note':self.note,\
+      'checkinDate':self.checkinDate, 'mozillian':self.mozillian}
+
 
 class Mozillian(SQLObject):
 
@@ -13,9 +18,18 @@ class Mozillian(SQLObject):
   engagement = ForeignKey('Engagement')
   checkins = MultipleJoin('Checkin')
 
+  def toDict(self):
+    return {'nickname':self.nickname, 'email':self.email,\
+      'engagement':self.engagement,\
+      'checkins':map(lambda c: c.toDict(), Checkin.selectBy(mozillian = self.id))}
+
 
 class Engagement(SQLObject):
 
   numberOfDays = IntCol()
   numberOfHours = IntCol()
   startingDay = DateCol()
+
+  def toDict(self):
+    return {'numberOfDays':self.numberOfDays, 'numberOfHours':self.numberOfHours,\
+      'startingDay':self.startingDay}
